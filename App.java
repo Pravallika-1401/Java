1d2bac2dd9b6a5764b0d489158d6c91ad85e3933
Pravallika-1401 <pravallikach1401@gmail.com>
2025-07-18 21:57:43 +0530

    Task1

---
+++ App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) {
+        String cname = "PANGASINAN STATE UNIVERSITY";
+        String LastName = "Pravallika";
+        String FirstName = "Chevvuru";
+        String MiddleName = " ";
+        String Address = "Gachibowli Hyderabad";
+        String  DOB = "14/1/2004";
+        String Citizenship ="Indian";
+        String Birthplace ="Vizag";
+        String Civil_Status = "Single";
+        String Gender ="Female";
+       
+        String Religion = "Hindu";
+        String Father_Name = "Satish kumar";
+        String Occupation = "Business man";
+        System.out.println(cname);
+        System.out.println(LastName);
+        System.out.println(FirstName);
+        System.out.println(MiddleName);
+        System.out.println(Address);
+        System.out.println(DOB);
+        System.out.println(Citizenship);
+        System.out.println(Birthplace);
+        System.out.println(Civil_Status);
+        System.out.println(Gender);
+        System.out.println(Religion);
+        System.out.println(Father_Name);
+        System.out.println(Occupation);
+
+    }
+}
---
+++ car.java
@@ -0,0 +1,26 @@
+public class car {
+    public static void main(String[] args) {
+        String Engine_Type = "mHAWk 4 Cylinder";
+        String Displacement ="2184 cc";
+        String Max_Power ="130bhp@3750rpm";
+        String Max_Torque = "300Nm@1600-2800rpm";
+        int no_of_Cylinders = 4;
+        int Valves_Per_Cylinder = 4;
+        String Fuel_Supply_System = "CRDi";
+        String Turbo_Charger = "Yes";
+        String Transmission_Tpye ="Manual";
+        String Gearbox = "6-Speed";
+        String Drive_Type ="RWD";
+        System.out.println(Engine_Type);
+        System.out.println(Displacement);
+        System.out.println(Max_Power);
+        System.out.println(Max_Torque);
+        System.out.println(no_of_Cylinders);
+        System.out.println(Valves_Per_Cylinder);
+        System.out.println(Fuel_Supply_System);
+        System.out.println(Turbo_Charger);
+        System.out.println(Transmission_Tpye);
+        System.out.println(Gearbox);
+        System.out.println(Drive_Type);
+    }
+}
