a80f48cf80f30a89c0bda2eb06c3641aca44c552
Pravallika-1401 <pravallikach1401@gmail.com>
2025-07-25 16:50:08 +0530

    loops

---
+++ loops.java
@@ -0,0 +1,15 @@
+public class loops {
+    public static void main(String[] args) {
+        // for (int i=0;i<=5;i++){
+        //     System.out.println(i);
+        // }        
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // while(a == 10){
+        //     System.out.println(a);
+        // }
+        do { 
+            System.out.println("a");
+        } while (true);
+    }
+}
