efe972d3c5be9fc997f5173a3cc0f8d3bce36f92
Pravallika-1401 <pravallikach1401@gmail.com>
2025-08-04 22:57:30 +0530

    Basic_codes

---
+++ print.java
@@ -0,0 +1,24 @@
+public class print {
+    public static void main(String[] args) {
+        //print
+        //System.out.println("Hello World");
+        //variables and datatypes
+        // int n = 5;
+        // String name = "Yash";
+        // System.out.println(name);
+        // System.err.println(n);
+        // loops
+        // if (1+2==3){
+        //     System.out.println("odd");
+        // }else{
+        //     System.out.println("even");
+        // }
+        String name = "Yash";
+
+        if (name.equals("Yash")){
+            System.out.println("boy");
+        }else{
+            System.out.println("not");
+        }
+    }
+}
